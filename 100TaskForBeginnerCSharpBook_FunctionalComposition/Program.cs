65325d13bbae24da5b40059f56ebf478c7129fcc
c0baltt <c0baltt2010@gmail.com>
2022-03-21 16:27:20 +0300

    Initial commit

---
+++ 100TaskForBeginnerCSharpBook_FunctionalComposition/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+namespace _100TaskForBeginnerCSharpBook_FunctionalComposition
+{
+    class Program
+    {
+
+        private static int balance = 0;
+        private static int[] coinsQuantity = { 0, 0, 0, 0 };
+        private static int[] coinsValues = { 1, 2, 5, 10 };
+        private static string[] names = { "Шоколадка", "Газировка" };
+        private static int[] prices = { 70, 60 };
+        private static PaymentType payment = PaymentType.Card;
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"Баланс {balance}");
+
+                string command = ReadCommand();
+                ExecuteCommand(command);
+
+                Console.ReadKey();
+
+            }
+        }
+
+        private static void ExecuteCommand(string command)
+        {
+            if (command == "AddMoney")
+            { 
+                switch (payment)
+                {
+                    case PaymentType.Coins:
+                        for (int i = 0; i < coinsValues.Length; i++)
+                        {
+                            Console.WriteLine($"Сколько монет номиналом {coinsValues[i]} вы хотите внести");
+                            int count = ReadInt();
+                            coinsQuantity[i] += count;
+                            balance += count * coinsValues[i];
+                        }
+                        break;
+                    case PaymentType.Card:
+                        Console.WriteLine("Сколько снять с вашей карты?");
+                        int balanceDelta = ReadInt();
+                        balance += balanceDelta;
+                        Console.WriteLine("Баланс успешно пополнен");
+                        break;
+                }
+            }
+            else if (command == "GetChange")
+            {
+                balance = 0;
+            }
+            else if (command.StartsWith("BuyGood"))
+            {
+                //Разбиение строки на еденицы данных
+                string[] rawData = command.Substring("BuyGood ".Length).Split(' ');
+
+                //сопоставление  этих данных с переменными (и их типами)
+                if (rawData.Length != 2)
+                {
+                    Console.WriteLine("Неправильные аргументы команды");
+                    return;
+                }
+
+                int id = 0;
+                if(!MapParametr(rawData, out id, BuyGoodParameter.Id))
+                {
+                    return;
+                }
+
+                int count = 0;
+                if (!MapParametr(rawData, out count, BuyGoodParameter.Count))
+                {
+                    return;
+                }
+
+                //Проверка корректности этих данных на основе текущей модели.
+                if (!Exist(id))
+                {
+                    Console.WriteLine("Такого товара нет");
+                    return;
+                }
+
+                if(!IsAvalableInQuantity(id, count))
+                {
+                    Console.WriteLine("Нет такого количества");
+                    return;
+                }
+
+                //Выполнение
+                int totalPrice = GetTotalPrice(GetPrice(id), count);
+                
+                if (balance >= totalPrice)
+                {
+                    balance -= totalPrice;
+                    avalibleQuantity[id] -= count;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Команда не определена");
+            }
+        }
+
+        private static bool Exist(int id)
+        {
+            return id > 0 && id < names.Length;
+        }
+
+        private static void ValidateId(int id)
+        {
+            if (!Exist(id))
+            {
+                throw new ArgumentOutOfRangeException("id");
+            }
+        }
+
+        private static int GetPrice(int id)
+        {
+            ValidateId(id);
+            return names[id];
+        }
+
+        private static int GetPrice(int id)
+        {
+            ValidateId(id);
+            return avalibleQuantity[id];
+        }
+
+        private static int GetTotalPrice(int price, int count)
+        {
+            return price * count;
+        }
+
+        private static int ReadInt()
+        {
+            int result = 0;
+
+            while (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Вы ввели не число!");
+            }
+
+            return result;
+        }
+
+        private static bool MapParametr(string[] rawParams, out int containter, BuyGoodParameter parameter)
+        {
+            int index = (int)parameter;
+            string name = Enum.GetName(typeof(BuyGoodPfrfmeter), parameter);
+
+            if (!int.TryParse(rawParams[index], out containter))
+            {
+                Console.WriteLine($"Ошибка в параметре {name}, он должен быть числом");
+                return false;
+            }
+        }
+        enum BuyGoodParameter
+        {
+            Id = 0,
+            Count = 1
+        }
+    }
+
+    enum PaymentType
+    {
+        Coins,
+        Card
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+100TaskForBeginnerCSharpBook_FunctionalComposition
